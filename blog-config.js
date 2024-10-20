7b9089b3e430225608667d52cd6a65d011a2c79e
최재훈 <jaehun341012@gmail.com>
2024-10-20 18:12:51 +0900

    setting

--- blog-config.js
+++ blog-config.js
@@ -6,17 +6,17 @@ module.exports = {
   links: {
     github: "https://github.com/nuheajiohc"
   }
-//,
-  // See https://giscus.app/
- // giscus: {
- //   repo: "unheajiohc/nuheajiohc.github.io",
- //   repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=",
-  //  category: "Comments",
- //   categoryId: "DIC_kwDOFguqvs4ChwGy",
-  //  mapping: "pathname",
-   // strict: "0",
-   // reactionsEnabled: "1",
-   // inputPosition: "bottom",
-   // lang: "en",
-  //},
+,
+   See https://giscus.app/
+  giscus: {
+    repo: "unheajiohc/nuheajiohc.github.io",
+    repoId: "MDEwOlJlcG9zaXRvcnkzNjk4NjMzNTg=",
+    category: "Comments",
+    categoryId: "DIC_kwDOFguqvs4ChwGy",
+    mapping: "pathname",
+    strict: "0",
+    reactionsEnabled: "1",
+    inputPosition: "bottom",
+    lang: "en",
+  },
 }
